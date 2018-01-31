5f492a49592a1865b02de0c74f12e1c563b043b8
EveryOS EveryPlatform <j.gronn4159@lakewoodcityschools.org>
2018-01-31 09:51:26 -0800

    Update encoder.lua

--- mzip/encoder.lua
+++ mzip/encoder.lua
@@ -1,3 +1,25 @@
+local function toITable(mytable)
+  local tbl = {}
+  local ids = {}
+  for k, v in pairs(mytable) do
+    if type(k) == "number" then
+      local tryIndex = 1
+      while tryIndex <= #ids do
+        if k >= ids[tryIndex] then table.insert(ids, tryIndex, k) break end
+        if tryIndex == #ids then table.insert(ids, tryIndex, k) break end
+        tryIndex = tryIndex + 1
+      end
+      if #ids == 0 then ids[1] = k end
+    else
+      tbl[k] = v
+    end
+  end
+  for k, v in ipairs(ids) do  --IDs are reversed
+    tbl[k] = mytable[ids[#ids - k + 1]]
+  end
+  return tbl
+end
+
 local function getChars(s)
   local c = {}
   local i = 1
@@ -17,6 +39,13 @@ local function getFreq(tC)
     if not groups[v] then groups[v] = {} end
     table.insert(groups[v], k)
   end
+  groups = toITable(groups)
+  for k, v in ipairs(groups) do
+    for k, v in ipairs(k) do
+      table.insert(f, v)
+    end
+  end
+  return f
 end
 
 return function(s)
