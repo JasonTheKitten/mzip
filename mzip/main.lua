e5594b02ff1a5c9d693dd84ae3f48714c8b39bab
EveryOS EveryPlatform <j.gronn4159@lakewoodcityschools.org>
2018-02-14 15:38:32 -0500

    Update main.lua

--- mzip/main.lua
+++ mzip/main.lua
@@ -4,11 +4,15 @@ if not arg1 then
   error("Please use correctly", -1)
 end
 
+local function dwrite(mtext)
+  local f = io.open("G", "w")
+  print(mtext)
+  f:write(mtext)
+  f:close()
+end
+
 if (string.sub(arg1, 1, 1) == "/") or (string.sub(arg1, 1, 2) == "./") then
-  local encode = loadfile("mzip/encode.lua", _G)()
-  local decode = loadfile("mzip/decode.lua", _G)()
+  local compress = loadfile("mzip/compress.lua", _G)()
   local word = string.sub(arg1, 2, #arg1)
-  local str, padding = encode.encode(word)
-  local tree = encode.getEncodingTree(encode.getFreq(encode.getChars(word)))
-  print(decode.decode(str, tree, padding))
+  dwrite(compress.compress(word))
 end
