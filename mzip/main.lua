68db328932406e3d0fd2537d054d465eef782501
EveryOS EveryPlatform <j.gronn4159@lakewoodcityschools.org>
2018-02-14 20:48:33 -0500

    Update main.lua

--- mzip/main.lua
+++ mzip/main.lua
@@ -1,18 +1,15 @@
-local arg1 = ...
+local arg1 = [[/Hello
+
+\n{}
+]]--...
 
 if not arg1 then
   error("Please use correctly", -1)
 end
 
-local function dwrite(mtext)
-  local f = io.open("G", "w")
-  print(mtext)
-  f:write(mtext)
-  f:close()
-end
-
 if (string.sub(arg1, 1, 1) == "/") or (string.sub(arg1, 1, 2) == "./") then
   local compress = loadfile("mzip/compress.lua", _G)()
+  local decompress = loadfile("mzip/decompress.lua", _G)()
   local word = string.sub(arg1, 2, #arg1)
-  dwrite(compress.compress(word))
+  print(decompress.decompress(compress.compress(word)))
 end
