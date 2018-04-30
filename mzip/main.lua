f26d9f1ab85b1d03bb443692c36d8c2f983f2891
EveryOS <j.gronn4159@lakewoodcityschools.org>
2018-04-30 09:30:09 -0700

    Update

--- mzip/main.lua
+++ mzip/main.lua
@@ -36,34 +36,12 @@ local compress = mzipload("compress.lua")()
 local decompress = mzipload("decompress.lua")()
 
 if args[1] == "#run" then
-  local handle = http.get("https://pastebin.com/raw/"..args[2])
-  local result = handle:readAll()
+  local compressed = compress.compress(compress.fromFolder())
+  local handle = fs.open(args[2], "w")
+  handle:write(compressed)
   handle:close()
-  decompress.toFolder(decompress.decompress(result), ".temp")
-  shell.run(".temp/main.lua")
-  fs.delete(".temp")
-elseif args[1] == "#put" then
-    local key = "0ec2eb25b6166c0c27a394ae118ad829"
-    local text = compress.compress(compress.fromFolder(args[2]))
-    local response = http.post(
-      "https://pastebin.com/api/api_post.php",
-      "api_option=paste&"..
-      "api_dev_key="..key.."&"..
-      "api_paste_format=lua&"..
-      "api_paste_name="..textutils.urlEncode((args[3] or "")..".mzip").."&"..
-      "api_paste_code="..textutils.urlEncode(text)
-    )
-
-    if response then
-      local sResponse = response.readAll()
-        response.close()
-        local sCode = string.match( sResponse, "[^/]+$" )
-        print( "Uploaded as "..sResponse )
-    else
-        print( "Err." )
-    end
-elseif args[1] == "#get" then
-  local handle = http.get("https://pastebin.com/raw/"..args[2])
+elseif args[1] == "#extract" then
+  local handle = fs.open(args[2], "r")
   local result = handle:readAll()
   handle:close()
   decompress.toFolder(decompress.decompress(result), args[3])
