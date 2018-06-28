4d2a427502f79b7bef4bad99cd9de3cdd0fe7c4a
EveryOS <j.gronn4159@lakewoodcityschools.org>
2018-06-28 11:49:10 -0400

    Update compress.lua

--- mzip/compress.lua
+++ mzip/compress.lua
@@ -56,6 +56,9 @@ local function fromSegs(segs)
       else
         processN(in, "{")
         in = 0
+        if tonumber(segs[1]) then
+          rtn = rtn .. "\\"
+        end
         rtn = rtn..segs[1]
       end
       table.remove(segs, 1)
